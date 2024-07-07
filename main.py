ad2b7c65155fadb7cf37bf8c08e34a9e74a49ae4
Ivan <ivannewest@gmail.com>
2024-07-07 20:49:10 +0300

    v1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from utils import OKX
 if __name__ == '__main__':
     try:
         session = OKX()
+        user_input = input('Choice the desired option: [1] add wallets to whitelist [2] remove wallets from whitelist')
         session.add_to_whitelists()
         logger.success("done!")
     except Exception as e:
--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException, StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as ec
+from tqdm import tqdm 
 import sys 
-import ccxt 
 from loguru import logger
 
 with open("wallets.txt", "r") as f:
@@ -17,19 +17,12 @@ with open("config.json", "r") as f:
 
 WAIT_TIME = 10
 class OKX:
-    def __init__(self):
+    def __init__(self, driver=Driver(uc=True)):
         self.okx_url_login = "https://www.okx.com/ru/account/login"
-        self.driver = Driver(uc=True)
+        self.driver = driver 
         self._zero = 0
         self._len_wallets = len(WALLETS)
         self.wallets_batches = [WALLETS[i:i + 20] for i in range(0, len(WALLETS), 20)] 
-        self.okx_api = ccxt.okx(
-            {
-                "apiKey": CONFIG['apiKey'],
-                "secret": CONFIG['secret'],
-                "password": CONFIG['passwordOKX']
-            }
-        )
 
     @staticmethod
     def manual_login():
@@ -55,24 +48,26 @@ class OKX:
                 logger.error(f"Error in filling_addresses function: {error}") 
             
         time.sleep(0.3)
-        for i in range(0, len(WALLETS)-1):
-            btn = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/button")
+        for i in range(0, len(batch)-1):
+            btn = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/button/span")
             if btn:
                 btn.click()
             else:
                 self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/button").click()
                 self.scroll_window()
-            time.sleep(0.5)
+            time.sleep(1)
         
-        input_forms = [i for i in self.driver.find_elements(By.CLASS_NAME, "balance_okui-input-input") if i.accessible_name == 'Адрес / домен']
+        input_forms = [i for i in self.driver.find_elements(By.CLASS_NAME, "balance_okui-input-input") if i.accessible_name == 'Адрес']
         for wallet, form in zip(batch, input_forms):
             self._zero += 1 
             logger.info(f'add : {wallet} [{self._zero}/{self._len_wallets}]')
             form.send_keys(wallet)
             time.sleep(0.01)
             
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/div[3]/div/div/div/label/span[1]/input").click()
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/div[4]/div/div/div/button").click()
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]"
+                                           "/div/form/div[3]/div/div/div/label/span[1]/input").click()
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div"
+                                           "/div[2]/div/form/div[4]/div/div/div/button").click()
         time.sleep(1)
 
     def scroll_window(self, x: int = 0, y: int = 50, up: bool = False):
@@ -111,28 +106,33 @@ class OKX:
             if email_code:
                 break
 
-        self.driver.find_element(By.XPATH, "/html/body/div[7]/div/div[2]/div/div/div/div/div/div/div/form/div[1]/div[2]/div/div/div/div/div/div/input[2]").send_keys(email_code)
+        code_forms = self.driver.find_elements(By.XPATH, "//input[@placeholder='Ввести код']")
+        code_forms[0].clear()
+        code_forms[0].send_keys(email_code)
+        time.sleep(0.2)
+        totp = input("Enter 2fa code: ")
+        code_forms[1].clear()
+        code_forms[1].send_keys(totp)
 
-        two_fa = input('Enter 2FA code: ')
-        self.driver.find_element(By.XPATH, "/html/body/div[7]/div/div[2]/div/div/div/div/div/div/div/form/div[2]/div[2]/div/div/div/div/div/div/input[2]").send_keys(two_fa)
+        time.sleep(1)
+        self.wait_an_element(By.XPATH, """//*[@id="body"]/div[6]/div/div[3]/div/button[2]/span""").click()
         time.sleep(2)
-        self.driver.find_elements(By.XPATH, "/html/body/div[7]/div/div[3]/div/button[2]").click()
-        time.sleep(2)
-        
+
+    def add_batch_to_whitelist(self, batch):
+        self.driver.get(CONFIG['withdrawalLink'])
+        time.sleep(5)
+        while True:
+            self.filling_addresses(batch)
+            self.confirmations()
+            break
     def add_to_whitelists(self):
         self.driver.get(self.okx_url_login)
         time.sleep(5)
         self.manual_login()
         time.sleep(5)
-        self.driver.get(CONFIG['withdrawalLink'])
-        time.sleep(5)
-
-        for batch in self.wallets_batches:
-            while True:
-                self.filling_addresses(batch)
-                self.confirmations()
-                break
-            logger.info("60 seconds pause")
+        for batch in tqdm(self.wallets_batches):
+            self.add_batch_to_whitelist(batch)
+            logger.info('60s pause')
             time.sleep(60)
 
     def wait_an_element(self, by, element_selector: str, wait_time: int = 10):
@@ -142,18 +142,38 @@ class OKX:
         except TimeoutException:
             logger.error(f'Error while waiting for an element: {element_selector}')
             sys.exit(-1)
+    def delete_wallets_from_one_page(self):
+        rows = self.driver.find_elements(By.CLASS_NAME, "balance_okui-table-row")
+        ix = 0
+        with tqdm(total=len(WALLETS)) as pbar:
+            while ix < len(rows):
+                row = rows[ix]
+                address = [i for i in row.find_elements(By.CSS_SELECTOR, "div") if i.get_property("className") == "AddressList_address__e+1Hh"][0]
 
+                if address.text in WALLETS:
+                    logger.info(f"deleting address: {address}")
+                    row.find_elements(By.CLASS_NAME, "btn-content")[0].click()
+                    self.wait_an_element(By.XPATH, "/html/body/div[7]/div/div[3]/div/button[2]").click()
+                    pbar.update(1)
+                    time.sleep(2)
+                    ix = 0
+                    rows = self.driver.find_elements(By.CLASS_NAME, "balance_okui-table-row")
+                    time.sleep(2)
+                else:
+                    ix += 1
 
-# if __name__ == '__main__':
-#     try:
-#         session = OKX()
-#         session.add_to_whitelists()
-#         session.filling_addresses(WALLETS)
-
-#         logger.success("done!")
-#     except Exception as e:
-#         logger.error(f"error: {e}\n\n")
-
-
-
-
+    def delete_wallets(self):
+        self.driver.get(CONFIG['whitelistPage'])
+        time.sleep(2)
+        while True:
+            self.delete_wallets_from_one_page()
+            try:
+                next_ = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div[2]/ul/li[2]')
+                next_disabled = "balance_okui-pagination-disabled" in next_.get_property("className")
+                if next_disabled:
+                    break 
+                next_.click()
+                time.sleep(5)
+            except:
+                break
+        
