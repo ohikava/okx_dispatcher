ad119d313df18bcaf112eaa45a27db8de4909e8a
Ivan <ivannewest@gmail.com>
2024-07-15 12:38:44 +0300

    v2.0, wl add, wl delete, create dispatch config, dispatch

---
+++ dispatcher.py
@@ -0,0 +1,76 @@
+import ccxt 
+import random 
+import json 
+import os
+from loguru import logger
+import time 
+
+class Dispatcher:
+    def __init__(self, WALLETS, CONFIG, SECRETS) -> None:
+        self.WALLETS = WALLETS
+        self.CONFIG = CONFIG
+        self.SECRETS = SECRETS
+
+        self.exchange = ccxt.okx({
+            "apiKey": self.SECRETS['APIKEY'],
+            "secret": self.SECRETS['SECRET'],
+            "password": self.SECRETS['PASSWORDOKX']
+        })
+
+        self._min_sol_amount = float(CONFIG['minSolAmount'])
+        self._max_sol_amount = float(CONFIG['maxSolAmount'])
+        self._dispatch_config_path = CONFIG['dispatchConfigPath']
+        self._fee = float(CONFIG['fee'])
+        self._token = CONFIG['token']
+        self._token_chain = f"{CONFIG['token']}-{CONFIG['chain']}"
+
+    def generate_config(self):
+        self.dispatch_config = {}
+        for wallet in self.WALLETS:
+            solAmount = round(random.uniform(self._min_sol_amount, self._max_sol_amount), 2)
+            self.dispatch_config[wallet] = solAmount
+        
+        with open(self._dispatch_config_path, "w") as file:
+            json.dump(self.dispatch_config, file)
+        
+
+
+    def dispatch(self):
+        self.generate_config() 
+        self._dispatch()
+
+    def withdraw(self, amount: float, address: str):
+        params = {
+            "fee": self._fee,
+            "chain": self._token_chain
+        }
+
+        tx = self.exchange.withdraw(
+            self._token,
+            amount,
+            address,
+            params=params
+        )
+        return tx 
+
+    def _dispatch(self):
+        success_rate = 0
+        unsucessful_wallets = []
+        n = len(self.WALLETS)
+        for ix, wallet in enumerate(self.WALLETS):
+            sol_amount = self.dispatch_config[wallet]
+            try:
+                tx = self.withdraw(sol_amount, wallet)
+                wdId = tx.get("info", {}).get("wdId", None)
+                logger.success(f"{ix}/{n} successful withdraw {sol_amount} SOL, {wallet[:5]}, wdId: {wdId}")
+                success_rate += 1
+            except Exception as e:
+                logger.error(str(e))
+                unsucessful_wallets.append(wallet) 
+            
+            time.sleep(1)
+            
+        logger.info(f"success rate {success_rate / n}")
+        logger.error(f"unsucessful wallets: {str(unsucessful_wallets)}")
+
+                
--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from loguru import logger
-from utils import OKX
+from whitelister import Whitelister
+import json 
+from dispatcher import Dispatcher
+from dotenv import dotenv_values
 
+SECRETS = dotenv_values(".env")
 
 if __name__ == '__main__':
+    
     try:
-        session = OKX()
-        user_input = input('Choice the desired option: [1] add wallets to whitelist [2] remove wallets from whitelist')
-        session.add_to_whitelists()
-        logger.success("done!")
+        with open("wallets.txt", "r") as f:
+            WALLETS = [i.strip() for i in f.readlines()]
+
+        with open("config.json", "r") as f:
+            CONFIG = json.load(f)
+
+        dispatcher = Dispatcher(WALLETS=WALLETS, CONFIG=CONFIG, SECRETS=SECRETS)
+        session = None 
+        while True:
+            logger.info("""сhoice the desired option:\n[1] add wallets to whitelist\n[2] remove wallets from whitelist\n[3] randomly dispatch\n[q] quit\n""")
+            user_input = input("your input: ")
+            
+            if user_input == str(1):
+                if not session:
+                    session = Whitelister(WALLETS=WALLETS, CONFIG=CONFIG)
+                session.add_to_whitelists()
+                logger.info('completed...')
+            elif user_input == str(2):
+                if not session:
+                    session = Whitelister(WALLETS=WALLETS, CONFIG=CONFIG)
+                session.delete_wallets()
+                logger.info('completed...')
+            elif user_input == str(3):
+                dispatcher.dispatch()
+                logger.info('completed...')
+            elif user_input == 'q':
+                break 
+            else:
+                logger.error("incorrect option. Try again")
     except Exception as e:
         logger.error(f"error: {e}\n\n")
